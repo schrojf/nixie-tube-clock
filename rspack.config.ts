fb1101cd429ce8fde862485838fdc016f0348d79
Viliam Schrojf <viliam@schrojf.sk>
2026-06-17 00:36:02 +0200

    Initial commit

---
+++ rspack.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from '@rspack/cli';
+import { rspack, type SwcLoaderOptions } from '@rspack/core';
+
+export default defineConfig({
+  entry: {
+    main: './src/index.ts',
+  },
+  target: ['browserslist:last 2 versions, > 0.2%, not dead, Firefox ESR'],
+  resolve: {
+    extensions: ['...', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        type: 'css/auto',
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset',
+      },
+      {
+        test: /\.(?:js|mjs|cjs|ts|mts|cts)$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              detectSyntax: 'auto',
+            } satisfies SwcLoaderOptions,
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: './index.html',
+    }),
+  ],
+});
